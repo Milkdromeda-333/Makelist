71436c1e022618dadd7cbc7ce7bec51ae78b58e0
Anjanique <anjaniquemackey@gmail.com>
2023-03-19 18:19:42 -0700

    adds auth page - no functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
 import { useState } from 'react'
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import Navbar from './components/Navbar';
+import './styles.css'
 
 function App() {
 
+  const [isDarkModeActive, setIsDarkModeActive] = useState(localStorage.getItem('theme') || true);
+
+  const toggleDarkMode = () => {
+    // if (isDarkModeActive) {
+    //   setIsDarkModeActive(prev => !prev);
+    //   localStorage.setItem('theme', isDarkModeActive);
+    // } else {
+    // }
+    setIsDarkModeActive(prev => !prev);
+    localStorage.setItem('theme', isDarkModeActive);
+    console.log(localStorage.getItem('theme'))
+  } 
+
   return (
-    <div>
-      <Auth />
+    <div className={`font-font ${isDarkModeActive ? 'dark' : null}`}>
+      <Navbar isThemeDark={isDarkModeActive} toggleFunc={toggleDarkMode} />
+      <Auth isThemeDark={isDarkModeActive} />
     </div>
   )
 }
---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+
+export default function LoginForm({ isUserLoggingIn, setIsUserLoggingIn }) {
+
+    const defaultFormInputs = {
+        username: "",
+        password: ""
+    }
+
+    const [userInput, setUserInput] = useState(defaultFormInputs);
+    
+
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+        
+        setUserInput(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(userInput);
+        setUserInput(defaultFormInputs);
+    }
+    
+    return (
+        <form
+            className="
+            flex flex-col gap-4
+            w-full
+            md:w-1/3"
+        >
+
+            <h1 className="text-center text-xl md:text-3xl">Make lists. Yup, it's that simple.</h1>
+
+            <label htmlFor="username">Username:</label>
+            <input 
+                type="text"
+                name="username"
+                value={userInput.username}
+                onChange={handleChange}
+                autoComplete="username"
+                className="border p-2 dark:text-dark-blue"
+            />
+            <label htmlFor="username">Password:</label>
+
+            <input 
+                type="password"
+                name="password"
+                value={userInput.password}
+                onChange={handleChange}
+                autoComplete={isUserLoggingIn ? "current-password":"new-password"}
+                className="border p-2 dark:text-dark-blue"
+            />
+
+            <button
+                className="
+                bg-apple text-white
+                py-2
+                hover:bg-apple-shade
+                dark:bg-dark-blue dark:hover:bg-dark-blue-shade"
+
+                onClick={handleSubmit}
+            >
+                {isUserLoggingIn ? "Sign in." : "Sign up."}
+            </button>
+
+            <button onClick={(e) =>{
+                e.preventDefault();
+                setIsUserLoggingIn(prev => !prev);
+            }}
+                className="decoration-apple-shade
+                w-fit
+                underline
+                hover:decoration-apple-shade
+                dark:decoration-dark-blue dark:hover:text-gray-200
+                hover:text-zinc-600"
+            >
+                {isUserLoggingIn ? "No account? Sign up." : "Already signed up? Log in."}
+            </button>
+
+        </form>
+    )
+}
