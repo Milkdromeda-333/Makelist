0f922555fcd3e7bbd937dede816516e00f93c4c8
Anjanique <anjaniquemackey@gmail.com>
2023-03-29 15:22:45 -0700

    chnages link in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="bg-plum mt-auto h-10 text-white dark:bg-dark-blue grid place-content-center">
             <span>
                 Developed by&nbsp;
-                <a href="https://linktr.ee/milkdromeda333" target="_blank"
+                <a href="https://milkdromeda-333.github.io/anjaniques-links/" target="_blank"
                     className="underline hover:text-pink dark:hover:text-gray-300"
                 >Anjanique</a>
                 🌍
