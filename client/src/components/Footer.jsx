b7a77e73c6e5be126ee46bc3e88c8c13a4b5ba5c
Anjanique <anjaniquemackey@gmail.com>
2023-05-02 20:47:27 -0700

    more small changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="bg-plum mt-auto h-10 text-white dark:bg-dark-blue grid place-content-center">
             <span>
                 Developed by&nbsp;
-                <a href="https://milkdromeda-333.github.io/anjaniques-links/" target="_blank"
+                <a href="https://anjanique-mackey.netlify.app/" target="_blank"
                     className="underline hover:text-pink cursor-pointer dark:hover:text-gray-300"
                 >Anjanique</a>
                 🌍
--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -110,10 +110,10 @@ export default function Item({ item, listId, setUserLists }) {
                 rounded px-2 m-2 cursor-pointer
                 ${
                     isChecked ?
-                    "bg-plum text-plum-tone line-through hover:bg-[#34272b] dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-[#323b49]"
+                    "bg-plum text-plum-tone line-through hover:bg-[#241b1d] dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-[#323b49]"
                     : "hover:bg-[#e3b2b3] dark:hover:bg-[#0f516e]"
                 }
-                ${isEditing && "no-underline flex-col my-2 px-0 hover:bg-initial dark:hover:bg-gray-700"}
+                ${isEditing && "no-underline flex-col my-2 px-0 hover:bg-plum dark:hover:bg-gray-700"}
             `}
         >
             <div
